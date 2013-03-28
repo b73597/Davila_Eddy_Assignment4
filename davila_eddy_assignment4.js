b1cfbc39f51cdd630f9aeb433af6ecd3e600daf3
Eddy Davila <EdDavila@fullsail.edu>
2013-03-28 12:04:23 -0400

    1st Problem Solved

--- davila_eddy_assignment4.js
+++ davila_eddy_assignment4.js
@@ -4,10 +4,10 @@
 
 //Visual Frameworks Library Functions
 
-// REVEALING MODULE PATTER EXERCISE NOT A SOLVED PROBLEM. EXERCISE ONLY
+//REVEALING MODULE PATTER EXERCISE NOT A SOLVED PROBLEM. EXERCISE ONLY
 var myLibrary = function(){
     
-    // CHECK NUMERIC F
+/*    // CHECK NUMERIC F
     var checkNum = function(val){
         if (isNaN(val)) {
             return false;
@@ -29,16 +29,50 @@ var myLibrary = function(){
 
 
     // CHECK STRING PATTERN
+    var checkString = function(val){
+        
+        var strLen = val.length;
+        for (var i=0; i <=strLen ;  i++) {
+            if (val.substring(i, i+1) === "-") {
+                return i+1;
+            }
+        }
+    }*/
+    // END OF REVEALING MODULE PATTER EXERCISE NOT A SOLVED PROBLEM. EXERCISE ONLY
+    
+    
+    //ASSIGNMENT 4
+    
+    //CHECK STRING FUNCTIONS: verifyNum, verifyEMail, verifyUrl, capFirstLet.
+    // CHECK VALID PHONE NUMBER
+    var verifyNum = function (string) {
+		var pattern = /\d{3}-\d{3}-\d{4}/;
+		return pattern.test(string);
+            
+    }
+    
 
     return {
-        "checkNum": checkNum,
-        "areyouEd": areyouEd
+        
+        //REVEALING MODULE PATTER EXERCISE NOT A SOLVED PROBLEM. EXERCISE ONLY
+        //"checkNum": checkNum,
+        //"areyouEd": areyouEd,
+        //"checkString": checkString,
+        //END OF REVEALING MODULE PATTER EXERCISE NOT A SOLVED PROBLEM. EXERCISE ONLY
+        "verifyNum": verifyNum,
+       
+        
         
     }
 }
 
 
 var newLib = new myLibrary();
-console.log("How the Module Works: " + newLib.checkNum(156));
-console.log("How the name function works: " + newLib.areyouEd("Ed"));
+//REVEALING MODULE PATTER EXERCISE NOT A SOLVED PROBLEM. EXERCISE ONLY
+//console.log("How the Module Works: " + newLib.checkNum(156));
+//console.log("How the name function works: " + newLib.areyouEd("Ed"));
+//console.log("The dash is in posotion: " + newLib.checkString("1234-56"));
+//END OF REVEALING MODULE PATTER EXERCISE NOT A SOLVED PROBLEM. EXERCISE ONLY
+console.log("Valid entry is:" + newLib.verifyNum("123-456-7890"));
+
 
